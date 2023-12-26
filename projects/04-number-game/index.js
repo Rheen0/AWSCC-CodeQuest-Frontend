abde21af0e09c24b0d751b014e967f4a5fa995f7
Rhandie <76822279+Rheen0@users.noreply.github.com>
2023-12-26 15:23:24 +0800

    Updated blog website contents

--- projects/04-number-game/index.js
+++ projects/04-number-game/index.js
@@ -3,11 +3,11 @@ setTimeout(() => {
     container.classList.add('active');
 }, 500);
 
-const music = document.getElementById("music");
-const mute = document.getElementById("mute-btn");
-mute.addEventListener("click", function () {
-    music.setAttribute("muted", "");
-});
+// const music = document.getElementById("music");
+// const mute = document.getElementById("mute-btn");
+// mute.addEventListener("click", function () {
+//     music.setAttribute("muted", "");
+// });
 //
 
 const guessNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
